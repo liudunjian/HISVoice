a031fa4e738fc92c3083b957c007aa7b74c1675b
liudunjian <liudunjian@hisense.com>
2018-09-29 14:33:27 +0800

    solve redefinition

--- ios/RNAlmoHisvoice.h
+++ ios/RNAlmoHisvoice.h
@@ -1,8 +1,10 @@
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import <React/RCTBridgeModule.h>
-#endif
+//#if __has_include("RCTBridgeModule.h")
+//#import "RCTBridgeModule.h"
+//#else
+//#import <React/RCTBridgeModule.h>
+//#endif
+//#import <React/RCTBridge.h>
+
 #import <React/RCTEventDispatcher.h>
 #import "HSSpeechRecognizer.h"
 
